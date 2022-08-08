a4686f8bcad0441f2e4888ff7641c58bc47c96c0
Anderson V. Dávila Guerrero <avdavilag@utn.edu.ec>
2022-08-08 00:46:37 -0500

    #4 #5 Seeding tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);  
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
